22092510d093d0a77c298b3ebe450b406eabb1bb
Nova <dineshkumar27293112@gmail.com>
2026-03-23 13:39:40 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,3 +4,9 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const jobUrl = import.meta.env.VITE_SUPABASE_JOB_URL
+const jobKey = import.meta.env.VITE_SUPABASE_JOB_KEY
+
+// Export the second client with a different name
+export const supabaseJob = createClient(jobUrl, jobKey)
